7a3a85e085dcec8e81b8f8042db081db5809c02c
mlambers <31807394+mlambers@users.noreply.github.com>
2019-02-11 18:28:02 +0700

    Commit #3 [Version 0.2b]

--- Laugh.lua
+++ Laugh.lua
@@ -1,43 +1,39 @@
 -----------------
--- Version 0.1 --
+-- Version 0.2b --
 -----------------
 
-local Laugh = {}
+local Laugh = {
+	OptionEnable = Menu.AddOption({"mlambers", "Laugh Spam"}, "1. Enable", "Enable/Disable this script."),
+	LastLaugh = 0,
+	NeedInit = true
+}
 
-Laugh.optionEnable = Menu.AddOption({ "mlambers", "Laugh Spam" }, "1. Enable Laugh", "Enable/Disable this script.")
-
-Laugh.lastlaugh = 0
-Laugh.NeedInit = true
 local myHero = nil
 
 function Laugh.OnScriptLoad()
 	myHero = nil
-	Laugh.lastlaugh = 0
+	Laugh.LastLaugh = 0
 	Laugh.NeedInit = true
+	
+	Console.Print("\n=============================================\n")
+	Console.Print("Script: Laugh Spam | Callback: OnScriptLoad\n")
+	Console.Print("Date & Time: " .. (os.date("%Y-%m-%d %I:%M %p")))
+	Console.Print("=============================================\n\n")
 end
 
 function Laugh.OnGameEnd()
 	myHero = nil
-	Laugh.lastlaugh = 0
-	Laugh.NeedInit = true
-end
-
-function Laugh.OnGameStart()
-	if myHero == nil then
-		myHero = Heroes.GetLocal()
-	end
-	
-	Laugh.lastlaugh = 0
+	Laugh.LastLaugh = 0
+	collectgarbage("collect")
 	Laugh.NeedInit = true
 end
 
 function Laugh.OnUpdate()
-	if Engine.IsInGame() == false then return end
-	if Menu.IsEnabled(Laugh.optionEnable) == false then return end
-	if GameRules.GetGameState() < 4 then return end
-	if GameRules.GetGameState() > 5 then return end
-	
+	if Menu.IsEnabled(Laugh.OptionEnable) == false then return end
+
 	if Laugh.NeedInit == true then
+		Laugh.LastLaugh = 0
+	
 		if myHero == nil then
 			myHero = Heroes.GetLocal()
 		end
@@ -46,11 +42,12 @@ function Laugh.OnUpdate()
 	end
 	
 	if myHero == nil then return end
+	
 	if Entity.IsAlive(myHero) == false then return end
 	
-	if Laugh.lastlaugh < GameRules.GetGameTime() then
+	if Laugh.LastLaugh < GameRules.GetGameTime() then
 		Engine.ExecuteCommand("say /laugh")
-		Laugh.lastlaugh = GameRules.GetGameTime() + 15.0
+		Laugh.LastLaugh = GameRules.GetGameTime() + 15.0
 	end
 end
 
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Hake.me-SpamLaugh Version 0.1
+# Hake.me-SpamLaugh Version 0.2b
 
 Spam /laugh every 15 second.
